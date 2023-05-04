b0c8d1e17a5812d7f698a5744150f1cbe995ab25
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-05-04 14:31:52 +0200

    Jest test fungerar och går igenom

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -21,5 +21,5 @@ test('peek on stack with two or more elements returns the top element', () => {
 test('peek on stack with 5 elements', () => {
   stack.push(5);
   expect(stack.peek()).toBeDefined();
-  expect(stack.peek()).toBe(4);
+  expect(stack.peek()).toBe(5);
 });
